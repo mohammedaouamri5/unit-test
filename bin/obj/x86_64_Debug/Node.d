4dd9f12667e6f054d207484b4d8097b64838e6a5
mohammedaouamri5 <mohammedaouamri5@gmail.com>
2024-11-20 15:59:30 +0000

    build system done

--- bin/obj/x86_64_Debug/Node.d
+++
@@ -1,4 +0,0 @@
-bin/obj/x86_64_Debug/Node.o: Source/Node.cpp /usr/include/stdc-predef.h \
- Source/Node.hpp
-/usr/include/stdc-predef.h:
-Source/Node.hpp:
--- bin/obj/x86_64_Debug/helperfunctionandmecro.d
+++
@@ -1,3 +0,0 @@
-bin/obj/x86_64_Debug/helperfunctionandmecro.o: \
- Source/helperfunctionandmecro.cpp /usr/include/stdc-predef.h
-/usr/include/stdc-predef.h:
